3a258983bfe7549a2c8151bd30688a4545106cb7
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-10-01 20:38:39 +0200

    Restucture and redesign 2FA provider selection and challenge page
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- core/templates/twofactorselectchallenge.php
+++ core/templates/twofactorselectchallenge.php
@@ -1,6 +1,6 @@
 <div class="warning">
 	<h2 class="two-factor-header"><?php p($l->t('Two-factor authentication')) ?></h2>
-	<p><?php p($l->t('Enhanced security is enabled for your account. Please authenticate using a second factor.')) ?></p>
+	<p><?php p($l->t('Enhanced security is enabled for your account. Choose a second factor for authentication:')) ?></p>
 	<?php if ($_['providerMissing']): ?>
 	<p>
 		<strong><?php p($l->t('Could not load at least one of your enabled two-factor auth methods. Please contact your admin.')) ?></strong>
@@ -10,31 +10,42 @@
 		<ul>
 			<?php foreach ($_['providers'] as $provider): ?>
 				<li>
-					<a class="button two-factor-provider"
+					<a class="two-factor-provider"
 					   href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('core.TwoFactorChallenge.showChallenge',
 										[
 											'challengeProviderId' => $provider->getId(),
 											'redirect_url' => $_['redirect_url'],
 										]
 									)) ?>">
-						<?php p($provider->getDescription()) ?>
+						<img src="<?php p(image_path('twofactor_u2f', 'app.svg')) ?>" alt="" />
+						<div>
+							<h3><?php p($provider->getDisplayName()) ?></h3>
+							<p><?php p($provider->getDescription()) ?></p>
+						</div>
 					</a>
-					<div>Display name: <?php p($provider->getDisplayName()) ?></div>
-					<div>Description: <?php p($provider->getDescription()) ?></div>
-					<div>Icon: <?php p("some/icon.svg") ?></div>
 				</li>
 			<?php endforeach; ?>
+			<?php if (!is_null($_['backupProvider'])): ?>
+				<li>
+					<a class="two-factor-provider two-factor-secondary" href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('core.TwoFactorChallenge.showChallenge',
+						[
+							'challengeProviderId' => $_['backupProvider']->getId(),
+							'redirect_url' => $_['redirect_url'],
+						]
+					)) ?>">
+						<div>
+							<p><?php p($l->t('Use backup code')) ?></p>
+						</div>
+					</a>
+				</li>
+			<?php endif; ?>
+			<li>
+				<a class="two-factor-provider two-factor-secondary" href="<?php print_unescaped($_['logout_url']); ?>">
+					<div>
+						<p><?php p($l->t('Cancel log in')) ?></p>
+					</div>
+				</a>
+			</li>
 		</ul>
-	</p>
-	<p class="two-factor-link">
-		<a class="button" href="<?php print_unescaped($_['logout_url']); ?>"><?php p($l->t('Cancel log in')) ?></a>
-		<?php if (!is_null($_['backupProvider'])): ?>
-		<a class="button" href="<?php p(\OC::$server->getURLGenerator()->linkToRoute('core.TwoFactorChallenge.showChallenge',
-												[
-													'challengeProviderId' => $_['backupProvider']->getId(),
-													'redirect_url' => $_['redirect_url'],
-												]
-											)) ?>"><?php p($l->t('Use backup code')) ?></a>
-		<?php endif; ?>
 	</p>
 </div>
